a3e16b5c5c7045d32023b11eb27a2e40e7b80498
zack54 <shninder5454@gmail.com>
2017-10-11 03:33:27 +0200

    Fix the lab

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { swif: 'songTitle' };
+function updatePlaylist(playlist, name, song) {
+  playlist[name] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name];
+  return playlist;
+}
